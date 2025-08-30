842f7687fc156ee74be9d0bd1c2acf99382284ac
Caetano Bordin <hyperplayer2012@gmail.com>
2025-08-30 09:53:49 -0300

    Atualização Função Speak

--- main.js
+++ main.js
@@ -24,3 +24,9 @@ console.log("versão ml5:", ml5.version);
         console.log('Model Loaded');
     })
     .catch(err => console.error('Erro ao carregar o modelo:', err));
+
+    function speak() {
+        let synth = window.speechSynthesis;
+        let speakData1 = "A primeira previsão é " + prediction1;
+        let speakData2 = "E a segunda previsão é " + prediction2; 
+    }
